330990d52c9c65359b5d77dd07b8aa79a6a5c5b2
Siddhant Khot <khotsiddhant1008@gmail.com>
2024-10-06 13:00:10 +0530

    preparing to push

---
+++ script.js
@@ -0,0 +1,21 @@
+const buttons=document.querySelectorAll(".button")
+const body=document.querySelector("body")
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        if(e.target.id=='red'){
+            body.style.backgroundColor='red'
+        }
+        else if(e.target.id=="yellow")
+        {
+            body.style.backgroundColor='yellow'
+        }
+        else if(e.target.id=="green")
+        {
+            body.style.backgroundColor='green'
+        }else if(e.target.id=="blue")
+        {
+            body.style.backgroundColor='blue'
+        }
+    })
+})
